0dd2fb98750c7e09ce77b06e0c99c12581a4493f
adistefano <adistefano@ebay.com>
2020-11-25 23:57:31 +0000

    Upgraded to Java 8 Refactored GildedRose and removed the need for reflection

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,30 +1,42 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class GildedRose {
+	private enum Types {
+		NORMAL("normal", Normal::new),
+		AGED_BRIE("Aged Brie", Brie::new),
+		CONCERT_PASSES("Backstage passes to a TAFKAL80ETC concert", Backstage::new),
+		SULFURAS("Sulfuras, Hand of Ragnaros", BasicItem::new),
+		MANA_CAKE("Conjured Mana Cake", Conjured::new);
 
-	private static final Map<String, Class> itemTypes = new HashMap<String, Class>() {
-		{
-			put("normal", Normal.class);
-			put("Aged Brie", Brie.class);
-			put("Backstage passes to a TAFKAL80ETC concert", Backstage.class);
-			put("Sulfuras, Hand of Ragnaros", BasicItem.class);
-			put("Conjured Mana Cake", Conjured.class);
+		private final String name;
+		private final Supplier<Item> factory;
+
+		Types(String name, Supplier<Item> factory) {
+			this.name = name;
+			this.factory = factory;
 		}
-	};
+
+		static Optional<Item> createByName(String name) {
+			return Arrays.stream(values())
+					.filter(type -> type.name.equals(name))
+					.findAny()
+					.map(type -> type.factory.get());
+		}
+	}
 
 	public static Item createItem(String name, Long quality, Long daysRemaining) {
-		Class klass = (itemTypes.get(name) != null) ? itemTypes.get(name) : BasicItem.class;
-		try {
-			Item item = (Item) klass.newInstance();
+		return Types.createByName(name).map(item -> {
 			item.setDaysRemaining(daysRemaining);
 			item.setQuality(quality);
 			return item;
-		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		return null;
+		}).orElse(null);
 	}
 }
